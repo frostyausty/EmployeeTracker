98910dfcdd0e4bebaa5bee0e5a597e0adac9ad6b
frostyausty <priceaustin62@gmail.com>
2020-11-21 12:33:43 -0600

    test

--- utils/dbfunctions.js
+++ utils/dbfunctions.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2');
 const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'austyfbaby!62',
+    password: '',
     database: 'employees_db'
 });
 
